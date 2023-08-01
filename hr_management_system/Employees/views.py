506c6c9baf3aa205c7fa671b6531a662c85320cf
johnthapa1 <thapajohn2057@gmail.com>
2023-08-01 19:43:33 +0545

    URL Configured

--- hr_management_system/Employees/views.py
+++ hr_management_system/Employees/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+def employee_all(request):
+    return render(request, 'employees/employee_all.html')
+
+def employee_attendance(request):
+    return render(request,'employees/employee_attendance.html')
+
+def employee_departments(request):
+    return render(request, 'employees/employee_departments.html')
+
+def employee_designations(request):
+    return render(request, 'employees/employee_designations.html')
+
+def employee_holidays(request):
+    return render(request, 'employees/employee_holidays.html')
