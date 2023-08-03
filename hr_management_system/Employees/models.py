2ffe4aaccfbf4aed3511f0a9c654271a89b03c16
johnthapa1 <thapajohn2057@gmail.com>
2023-08-03 09:36:47 +0545

    EmployeeDepartment model added and migrations made

--- hr_management_system/Employees/models.py
+++ hr_management_system/Employees/models.py
@@ -1,9 +1,16 @@
 from django.db import models
 
+
+class EmployeeDepartment(models.Model):
+    employee_department_name = models.CharField(max_length=100)
+
+    class Meta:
+        db_table = "department_name"
+        ordering = ["-employee_department_name"]
 class EmployeeDetail(models.Model):
     employee_code = models.PositiveIntegerField(primary_key=True, auto_created=True)
     employee_name = models.CharField(max_length=30)
-    employee_designation = models.CharField(max_length=50)
+    employee_designation = models.ForeignKey(EmployeeDepartment, on_delete=models.CASCADE)
     employee_contact = models.CharField(max_length=10)
     employee_image = models.FileField(upload_to="images/employeeImage", blank=True, null=True)
     
