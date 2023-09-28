3009071acda2798e480f8ce355629a702735f1c0
johnthapa1 <thapajohn2057@gmail.com>
2023-09-28 19:22:28 +0545

    urls updates and viewing the employee detail in employee list

--- hr_management_system/Employees/urls.py
+++ hr_management_system/Employees/urls.py
@@ -2,7 +2,12 @@ from django.urls import path
 from Employees import views
 
 urlpatterns = [
-     path('add/', views.employee_all, name='employee_all'),
+    path('list/', views.employee_list, name='employee_list'),
+    path('add/', views.employee_add, name='employee_add'),
+    path('show/<int:id>/', views.employee_show, name='employee-show'),
+    path('edit/<int:id>/', views.employee_edit, name='employee-edit'),
+    path('delete/<int:id>/', views.employee_delete, name='employee-delete'),
+    
     path('attendance/', views.employee_attendance, name='employee_attendance'),
     path('designations/', views.employee_designations, name='employee_designations'),
     path('holidays/', views.employee_holidays, name='employee_holidays')
--- hr_management_system/Employees/views.py
+++ hr_management_system/Employees/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from Employees.forms import EmployeeCreateForm, AttendanceRecordCreateForm, EmployeeDesignationCreateForm
 from Employees.models import EmployeeDesignation, EmployeeDetail, AttendanceRecord
 # Create your views here.
@@ -7,7 +7,7 @@ from Employees.models import EmployeeDesignation, EmployeeDetail, AttendanceReco
 #     context = {"employeeDepartmentForm": employee_department_create_form, "title": "Employee Departments"}
 #     return render(request, 'employees/employee_departments.html', context)
 
-def employee_all(request):
+def employee_add(request):
     employee_create_form = EmployeeCreateForm()  #creating form class object
     context = {"employeeDetailForm": employee_create_form, "title": "Add Employee"}
     
@@ -17,16 +17,41 @@ def employee_all(request):
         designation_obj = EmployeeDesignation.objects.get(id=request.POST.get('employee_designation'))
         employee_contact= request.POST.get('employee_contact')
         
+        #method 1
         
-        employee_all_obj = EmployeeDetail()
-        employee_all_obj.employee_code = employee_code
-        employee_all_obj.employee_name = employee_name
-        employee_all_obj.employee_designation= designation_obj    # passing designation object (foreign key object)
-        employee_all_obj.employee_contact = employee_contact
-        employee_all_obj.save()
+        # employee_add_obj = EmployeeDetail()
+        # employee_add_obj.employee_code = employee_code
+        # employee_add_obj.employee_name = employee_name
+        # employee_add_obj.employee_designation= designation_obj    # passing designation object (foreign key object)
+        # employee_add_obj.employee_contact = employee_contact
+        # employee_add_obj.save()
         
-        
-    return render(request, 'employees/employee_all.html', context)
+        #method 2- storing data with form class object
+        employee_add=EmployeeCreateForm(request.POST)
+        if employee_add.is_valid():
+            employee_add.employee_designation=designation_obj
+            employee_add.save()
+            return redirect("employee_list")
+        return redirect("employee_add")
+         
+    return render(request, 'employees/employee_add.html', context)
+
+def employee_list(request):
+    list_of_employee= EmployeeDetail.objects.all()
+    context= {"data": list_of_employee }
+    return render(request, 'employees/employee_list.html', context)
+
+# def employee_show(request, id):
+#     return render(request, 'employees/employee_show.html')
+def employee_show(request, id):
+    employee = get_object_or_404(EmployeeDetail, id=id)
+    return render(request, 'employees/employee_show.html', {'employee': employee})
+
+def employee_edit(request, id):
+    return render(request, 'employees/employee_edit.html')
+
+def employee_delete(request, id):
+    return redirect(request, 'employee_list')
 
 def employee_attendance(request):
     return render(request,'employees/employee_attendance.html')
