20c54127ec164e62bcada865e598dd1e37b9be55
John <thapajohn2057@gmail.com>
2023-11-28 22:06:18 +0545

    project model updated

--- hr_management_system/Project/models.py
+++ hr_management_system/Project/models.py
@@ -1,17 +1,17 @@
 from django.db import models
-from django.contrib.auth.models import User
+from Employees.models import EmployeeDetail
 
 # Create your models here.
 class AssignProject(models.Model):
     project_name = models.CharField(max_length=100)
     project_deadline = models.DateTimeField()
     assigned_hours = models.DecimalField(max_digits=5, decimal_places=2)
-    assigned_to = models.ForeignKey(User, on_delete=models.CASCADE)
+    assigned_to = models.ForeignKey(EmployeeDetail, on_delete=models.CASCADE)
     assigned_date = models.DateTimeField()
     work_description = models.CharField(max_length=200)
 
     def __str__(self):
-        return f"{self.project_name} - Assigned to: {self.assigned_to.username}"
+        return f"{self.project_name} - Assigned to: {self.assigned_to.employee_name}"
     
     class Meta:
         db_table = "assign_project"
